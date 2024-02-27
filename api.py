0008f81c7abf10f5c4992352f509b4d5b20e0342
AshleyJubilee <ashleybjubilee@gmail.com>
2024-02-27 05:37:24 -0700

    api + exe

---
+++ api.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from pprint import pprint
+
+
+
+username = 'ashjubilee'
+
+url = "https://api.aimlab.gg/graphql"
+
+graphql_query = """query GetProfile($username: String) {
+        aimlabProfile(username: $username) {
+            username
+            id
+        }
+    }"""
+
+variables = {
+  "username": username,
+  "user": {
+                "id": ""
+            }
+}
+
+data = {'query': graphql_query,
+        "variables": variables}
+
+headers = {
+  'content-type': 'application/json'
+}
+
+response = requests.post(url=url, headers=headers, data=json.dumps(data))
+
+pprint(response.json())
