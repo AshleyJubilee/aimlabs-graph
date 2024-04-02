e33f47f3f28c99480f0e6b3635194b4e6c1410b4
AshleyJubilee <ashleybjubilee@gmail.com>
2024-04-02 12:44:33 -0600

    templates and views init

--- NinetyDayAim/tracker/views.py
+++ NinetyDayAim/tracker/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Username
 
 # Create your views here.
+def index(request):
+    return render(request, 'home.html',
+                  context={
+            "session": request.session.get("user"),
+        },)
+
+def userpage(request, username):
+    username = get_object_or_404(Username, username)
+    return render(request, "user.html", {
+        "username": username
+    })
